1e889925c12e905c0d43e206b1100e2a41df28fa
Scott <schepers.scott@hotmail.com>
2018-05-14 19:51:56 +0200

    Added: moving start spinning text, mousehand

--- js/main.js
+++ js/main.js
@@ -12,21 +12,34 @@ var config = {
 var game = new Phaser.Game(config);
 var width = game.config.width;
 var height = game.config.height;
+var startText;
+var startTextStartHeight = 540;
+var up;
 
 function preload() {
     this.load.image('BG', 'assets/bg.jpg');
     this.load.image('SLOTMACHINE', 'assets/slotmachine.jpg');
     this.load.image('SPIN', 'assets/spin-btn.png');
     this.load.image('START', 'assets/start-spinning.png');
+    this.load.image('MOUSEHAND', 'assets/mousehand.png');
 }
 
 function create() {
     this.add.image(width / 2, height / 2, 'BG');
     this.add.image(width / 2, height / 2, 'SLOTMACHINE');
     this.add.image(870, 616, 'SPIN');
-    this.add.image(870, 540, 'START');
+    startText = this.add.image(870, startTextStartHeight, 'START');
+    this.add.image(970, 675, 'MOUSEHAND');
+
 }
 
 function update() {
+    startMovement(startText, 10);
+}
 
+function startMovement(startText, maximumMovement) {
+    if (startText.y >= startTextStartHeight + maximumMovement / 2) up = false;
+    if (startText.y <= startTextStartHeight - maximumMovement / 2) up = true;
+    if (up) startText.y += 0.5;
+    else startText.y -= 0.5;
 }
