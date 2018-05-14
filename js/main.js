f30accc54c445a3a42ab15838c2254964d925a79
Scott <schepers.scott@hotmail.com>
2018-05-14 18:56:36 +0200

    Added: Slotmachine, spin button, start spinning text

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     scene: {
         preload: preload,
         create: create,
@@ -10,13 +10,21 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var width = game.config.width;
+var height = game.config.height;
 
 function preload() {
     this.load.image('BG', 'assets/bg.jpg');
+    this.load.image('SLOTMACHINE', 'assets/slotmachine.jpg');
+    this.load.image('SPIN', 'assets/spin-btn.png');
+    this.load.image('START', 'assets/start-spinning.png');
 }
 
 function create() {
-    this.add.image(400, 300, 'BG');
+    this.add.image(width / 2, height / 2, 'BG');
+    this.add.image(width / 2, height / 2, 'SLOTMACHINE');
+    this.add.image(870, 616, 'SPIN');
+    this.add.image(870, 540, 'START');
 }
 
 function update() {
